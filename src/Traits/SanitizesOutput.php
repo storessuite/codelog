414c05422e5233e54cc8be441f61285b31329f61
Saroj Kala Magar <sarojkalamagar@gmail.com>
2024-12-16 21:09:38 +0545

    use Datetime class instead of date()

--- src/Traits/SanitizesOutput.php
+++ src/Traits/SanitizesOutput.php
@@ -2,6 +2,8 @@
 
 namespace StoresSuite\Codelog\Traits;
 
+use Datetime;
+
 trait SanitizesOutput {
     public function sanitizeOutput(array $output): array{
         $commits = [];
@@ -9,7 +11,7 @@ trait SanitizesOutput {
         foreach($output as $line){
             $lineArray = explode('___', $line);
             $commits[] = [
-                'committed_at' => date($lineArray[0]),
+                'committed_at' => new Datetime($lineArray[0]),
                 'hash' => $lineArray[1],
                 'name' => $lineArray[2],
                 'email' => $lineArray[3],
