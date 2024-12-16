307db125407540e5f9bfd956f0482e13c0cbac41
Saroj Kala Magar <sarojkalamagar@gmail.com>
2024-12-16 21:05:19 +0545

    Switched to array index instead of object property.

--- src/Traits/TimesCommit.php
+++ src/Traits/TimesCommit.php
@@ -7,7 +7,7 @@ trait TimesCommit {
         foreach($commits as $index => &$commit){
             $commit['time_spent'] = $index === count($commits) - 1 ? 
                 $commit['time_spent'] = 'N/A' : 
-                $commits[$index + 1]->committed_at->diff($commit->committed_at)->format('%H:%I:%S');
+                $commits[$index + 1]['committed_at']->diff($commit['committed_at'])->format('%H:%I:%S');
         }
 
         return $commits;
